d5f9732954c70ce718db352c9274f337a57ee500
Andrew <31453677+Agamer22@users.noreply.github.com>
2025-05-07 13:25:32 +0300

    Add files via upload

---
+++ client.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//sockets
+#include <sys/socket.h>     // socket(), bind(), listen(), accept(), etc.
+#include <sys/un.h>         // sockaddr_un struct
+#include <unistd.h>         // close(), read(), write()
+
+
+#define MY_SOCK_PATH "/home/andrew/Desktop/ls2/ex2/mysock"
+
+
+struct sockaddr_un my_addr;
+
+
+int main(int argc, char **argv)
+{
+    printf("Please dont start a sequence with the number 0\n");
+
+    int sock = socket(AF_UNIX , SOCK_STREAM, 0);
+
+    memset(&my_addr, '\0', sizeof(struct sockaddr_un));
+    my_addr.sun_family = AF_UNIX;
+    strncpy(my_addr.sun_path, MY_SOCK_PATH, sizeof(my_addr.sun_path) -1);
+
+    int server_sock = connect(sock, (struct sockaddr *) &my_addr,sizeof(struct sockaddr_un));
+
+    printf("Client Side\n");
+    printf("To end sequence type 0\n");
+    printf("To exit type -1\n");
+
+
+    int a = 0, mo;
+char *buffer = malloc(100 * sizeof(char));
+
+while (a != -1)
+{
+    printf("Please type the next number\n");
+    scanf("%d", &a);
+    write(sock, &a, sizeof(int));
+
+    if (a == 0)
+    {
+        read(sock, &mo, sizeof(int));
+        printf("The mo of the numbers is %d\n", mo);
+
+        // Only try to read string if mo > 20
+        if (mo > 20)
+        {
+            memset(buffer, 0, 100);
+            ssize_t bytes = read(sock, buffer, 100);
+            if (bytes > 0)
+            {
+                printf("Output: %s\n", buffer);
+            }
+        }
+    }
+}
+
+    
+
+    close(sock);
+    unlink(MY_SOCK_PATH);
+        
+    return(0);
+}
+
+
+//TODO free buffer here and on the server
---
+++ main.c
@@ -0,0 +1,137 @@
+#include <complex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct
+{
+    int start;
+    int end;
+    int *max;
+    int my_max;
+    int number;
+    int *Array;
+}data_max;
+
+void find_max (data_max*);
+
+
+int main( int argc, char* argv[] )
+{
+    int *X , *buffer;
+    int size_of_X , i , j , n_threads , found , max ;
+    pthread_t *T;
+    data_max *S;
+
+    printf("Please type the size of the array:");
+    scanf("%d",&size_of_X);
+    printf("\n");
+
+    X = malloc (size_of_X*sizeof(int)); 
+    buffer = malloc (size_of_X*sizeof(int));
+    for ( i=0; i<size_of_X; i++)
+    {
+        printf("Please type No%d element of the array: ",i);
+        scanf("%d",&X[i]);
+    }
+
+    j=0;
+    for( i=1; i<=size_of_X; i++)
+    {
+        if( size_of_X % i == 0)
+        {
+            buffer[j] = i;
+            j++;
+        }
+    }
+
+    printf("How many threads do you want to find the max element?\n");
+    printf("Options: ");
+    for ( i=0; i<j; i++ )
+    {
+        printf("%d ",buffer[i]);
+    }
+    printf("\n");
+
+    found = 0;
+
+    while( found != 1 )
+    {
+        scanf("%d",&n_threads);
+        for( i=0; i<j; i++)
+        {
+            if( buffer[i] == n_threads )
+            {
+                found = 1;
+                break;
+                
+            }
+        }   
+        
+        if( found != 1)
+        {
+            printf("Please pick one of the options\n");
+        }
+             
+    }
+    
+    printf("\n");
+    free (buffer);    
+    
+
+    T = malloc (n_threads * sizeof(pthread_t));     //Thread array
+    S = malloc (n_threads*sizeof(data_max));        //Struct array
+    max = X[0];                                          //Init of max
+
+    for( i=0; i<n_threads; i++ )
+    {
+        if ( i==0 )
+        {
+            S[i].start = 0;
+            S[i].end = (size_of_X / n_threads) -1;
+            S[i].max = &max;
+            S[i].number = 1;
+            S[i].Array = X;
+        }
+        else if ( i>0)
+        {
+            S[i].start = S[i-1].start + ((size_of_X / n_threads));
+            S[i].end = S[i-1].end + ((size_of_X / n_threads));
+            S[i].max = &max;
+            S[i].number = i+1;
+            S[i].Array = X;
+        }
+        //printf("Created Thread\n");
+        pthread_create(&T[i], NULL , (void*) &find_max , &S[i]);
+        pthread_join(T[i], NULL);
+    }
+
+    printf("\nSo the total max is:%d\n\n", max);
+
+
+    free (T);
+    free (S);
+    free (X);
+    return (0);
+}
+
+
+
+void find_max(data_max* Data)
+{
+    printf("Thread No%d  Start:%d  End:%d\n", Data->number , Data->start , Data->end);
+    int i;
+    Data->my_max = Data->Array[Data->start];
+    for ( i=Data->start+1; i<Data->end; i++)
+    {
+        if( Data->Array[i] > Data->my_max )
+        {
+            Data->my_max = Data->Array[i];
+        }
+    }
+    printf("    I found local_max = %d\n\n",Data->my_max);
+    if( Data->my_max > *Data->max )
+    {
+        *Data->max = Data->my_max;
+    }
+}
